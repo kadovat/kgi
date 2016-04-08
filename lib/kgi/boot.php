9390e8e81cb354d8c425ed25028cb9e796e46f63
kad <kadovat@gmail.com>
2016-04-08 17:55:08 +0800

    format boot

--- lib/kgi/boot.php
+++ lib/kgi/boot.php
@@ -7,7 +7,7 @@ class Boot{
         $uri = $_SERVER['REQUEST_URI'];
         $uri = explode('?', $uri);
         $uri = $uri[0];
-		$suffix = empty(CUSTOM_SUFFIX) ? '.php' : '.' . CUSTOM_SUFFIX;
+        $suffix = empty(CUSTOM_SUFFIX) ? '.php' : '.' . CUSTOM_SUFFIX;
         if(substr($uri, - strlen($suffix)) != $suffix && substr($uri, -1) != '/'){
             throw new Kgi_Exception('bad url.');
         }
@@ -15,19 +15,19 @@ class Boot{
        	$uri = str_replace($suffix, '', $uri); 
        	$uri = substr($uri, 1); 
 
-		$pathArray = explode('/', $uri);
+        $pathArray = explode('/', $uri);
         if(self::$defaultIndex && empty($pathArray[0])){
-			$uri = 'index';
-			$action = '';
-			$className = 'App\\Ctrl\\Index';
-		}else{
-			$action = array_pop($pathArray) ;
-			$className = 'App\\Ctrl\\' . str_replace(' ', '\\', ucwords(implode(' ', $pathArray)));
-		}
+            $uri = 'index';
+            $action = '';
+            $className = 'App\\Ctrl\\Index';
+        }else{
+            $action = array_pop($pathArray) ;
+            $className = 'App\\Ctrl\\' . str_replace(' ', '\\', ucwords(implode(' ', $pathArray)));
+        }
         if(!file_exists(WEB_ROOT . '/' . str_replace('\\', '/', $className) . ".php" )){
             throw new KgiException("interface file missing,[{$uri}{$suffix}]");
-		}
- 
+        }
+
         
         $ctrl = new $className();
         $ctrl->setName(str_replace("/", ".", $uri));
